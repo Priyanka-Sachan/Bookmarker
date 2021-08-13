19a589cadec065b922a23c8993e37f674264d12f
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2021-08-13 16:20:54 +0530

    Add local storage

--- script.js
+++ script.js
@@ -1,8 +1,50 @@
 const form = document.getElementById('add-bookmark-form');
-const addBookmark = document.getElementById('add_bookmark');
+const bookmarksSection = document.getElementById('bookmarks-section');
 const wName = document.getElementById('w_name');
 const wUrl = document.getElementById('w_url');
 const note = document.getElementById('note');
+let bookmarks = [];
+
+// Fetch Bookmarks from local storage
+fetchBookmarks = () => {
+    if (localStorage.getItem('bookmarks')) {
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else {
+        bookmarks = [{
+            wName: 'Google',
+            wUrl: 'https://google.com',
+            note: ''
+        }];
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    buildBookmarks();
+}
+
+// Build bookmarks DOM
+buildBookmarks = () => {
+    bookmarks.forEach((bookmark) => {
+        const { wName, wUrl, note } = bookmark;
+        const card = document.createElement('div');
+        card.classList.add('card');
+        card.classList.add('bookmark');
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        const cardTitle = document.createElement('h5');
+        cardTitle.classList.add('card-title');
+        cardTitle.innerText = wName;
+        const cardSubtitle = document.createElement('h6');
+        cardSubtitle.classList.add('card-subtitle');
+        cardSubtitle.classList.add('mb-2');
+        cardSubtitle.classList.add('text-muted');
+        cardSubtitle.innerText = wUrl;
+        const cardText = document.createElement('p');
+        cardText.classList.add('card-text');
+        cardText.innerText = note;
+        cardBody.append(cardTitle, cardSubtitle, cardText);
+        card.append(cardBody);
+        bookmarksSection.append(card);
+    });
+}
 
 form.addEventListener('submit', function(event) {
     let isValid = form.checkValidity();
@@ -19,8 +61,9 @@ form.addEventListener('submit', function(event) {
             wUrl: wUrlValue,
             note: noteValue,
         };
-        console.log('Bookmark', bookmark);
-        event.preventDefault();
-        event.stopPropagation();
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     }
 }, false);
+
+fetchBookmarks();
