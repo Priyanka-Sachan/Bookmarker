8605f560fe757332ef24aae30deac5232d52fe60
Priyanka-Sachan <priyanka_1901cs43@iitp.ac.in>
2021-08-15 08:18:07 +0530

    Add delete function

--- script.js
+++ script.js
@@ -7,21 +7,33 @@ let bookmarks = [];
 
 // Fetch Bookmarks from local storage
 fetchBookmarks = () => {
-    if (localStorage.getItem('bookmarks')) {
-        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-    } else {
-        bookmarks = [{
-            wName: 'Google',
-            wUrl: 'https://google.com',
-            note: ''
-        }];
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        if (localStorage.getItem('bookmarks')) {
+            bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        } else {
+            bookmarks = [{
+                wName: 'Google',
+                wUrl: 'https://google.com',
+                note: ''
+            }];
+            localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+        }
+        buildBookmarks();
     }
-    buildBookmarks();
+    // Delete a bookmark
+deleteBookmark = (url) => {
+    bookmarks.forEach((bookmark, i) => {
+        if (bookmark.wUrl == url) {
+            bookmarks.splice(i, 1);
+        }
+    });
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
 }
 
 // Build bookmarks DOM
 buildBookmarks = () => {
+    //Remove all previous bookmarks
+    bookmarksSection.textContent = '';
     bookmarks.forEach((bookmark) => {
         const { wName, wUrl, note } = bookmark;
         const card = document.createElement('div');
@@ -29,9 +41,13 @@ buildBookmarks = () => {
         card.classList.add('bookmark');
         const cardBody = document.createElement('div');
         cardBody.classList.add('card-body');
+        const cardLink = document.createElement('a');
+        cardLink.setAttribute('href', wUrl);
+        cardLink.setAttribute('target', '_blank');
         const cardTitle = document.createElement('h5');
         cardTitle.classList.add('card-title');
         cardTitle.innerText = wName;
+        cardLink.append(cardTitle);
         const cardSubtitle = document.createElement('h6');
         cardSubtitle.classList.add('card-subtitle');
         cardSubtitle.classList.add('mb-2');
@@ -40,8 +56,13 @@ buildBookmarks = () => {
         const cardText = document.createElement('p');
         cardText.classList.add('card-text');
         cardText.innerText = note;
-        cardBody.append(cardTitle, cardSubtitle, cardText);
-        card.append(cardBody);
+        const closeIcon = document.createElement('i');
+        closeIcon.classList.add('close-icon');
+        closeIcon.classList.add('fa');
+        closeIcon.classList.add('fa-times');
+        closeIcon.setAttribute('onClick', `deleteBookmark('${wUrl}')`);
+        cardBody.append(cardLink, cardSubtitle, cardText);
+        card.append(closeIcon, cardBody);
         bookmarksSection.append(card);
     });
 }
